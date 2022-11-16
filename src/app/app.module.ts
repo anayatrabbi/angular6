f0602e3239823a1ace9eef8f8f040e23c9aefb14
mar.khan <mar.khan@asthait.com>
2022-11-16 10:09:16 +0600

    new

---
+++ src/app/app.module.ts
@@ -0,0 +1,121 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { Input, NgModule, Output , EventEmitter } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { Component } from '@angular/core';
+import { LoginComponent } from './auth/login.component';
+import { RegistraionComponent } from './auth/registraion.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// @Component({
+//   selector:"joker",
+//   template:`<div>
+//   <h2>{{data.setup}}</h2>
+//   <p [hidden]="data.hide">{{data.punchline}}</p>
+//   <a (click)="data.toggle()">hide</a> </div>`,
+
+// })
+// export class JokerComponent{
+//   //input to take input from anothee components
+//   @Input('joke')data : jokes
+//   //string interpolation
+//   constructor(){
+//   }
+// }
+
+// class jokes {
+//   public setup: string;
+//   public punchline: string;
+//   public hide: boolean;
+
+//   constructor(setup: string , punchline: string){
+//     this.setup = setup;
+//     this.punchline = punchline;
+//     this.hide = true;
+//   }
+
+//   toggle(){
+//     this.hide = !this.hide;
+//   }
+// }
+
+// @Component({
+//   selector:'joker-list',
+//   template:`
+//   <joker-form (jokerCreated)="addJoke($event)"></joker-form>
+//   <joker *ngFor="let j of jokes" [joke]="j"></joker>
+// `
+// })
+
+// export class JokerListComponents{
+//   jokes:jokes[];
+//   //() output property binding
+//   //[] input property bindings
+//   constructor(){
+//     this.jokes = [
+//       new jokes("What did the cheese say when it looked in the mirror?", "Hello-me (Halloumi)"),
+//       new jokes("What kind of cheese do you use to disguise a small horse?", "Mask-a-pony (Mascarpone)"),
+//       new jokes("A kid threw a lump of cheddar at me", "I thought ‘That’s not very mature’"),
+//     ];
+//   }
+
+//   addJoke(joke: any){
+//     this.jokes.unshift(joke)
+//   }
+// }
+
+// @Component({
+//   selector:"joker-form",
+//   template: `
+//   <div class="card card-block">
+//     <h4 class="card-title">Create Joke</h4>
+//     <div class="form-group">
+//       <input type="text"
+//              class="form-control"
+//              placeholder="Enter the setup"
+//              #setup>
+//     </div>
+//     <div class="form-group">
+//       <input type="text"
+//              class="form-control"
+//              placeholder="Enter the punchline"
+//              #punchline>
+//     </div>
+//     <button (click)="createJoke(setup.value , punchline.value)"> create</button>
+//   </div>
+//     `
+// })
+// class JokerForm{
+//   @Output() jokerCreated = new EventEmitter<jokes>();
+
+//   createJoke(setup: string , punchline: string ){
+//     this.jokerCreated.emit(new jokes(setup , punchline));
+//     console.log(setup)
+//     console.log(punchline)
+//   }
+//   // createJoke(setup: string , punchline: string){
+//   //   console.log(setup)
+//   //   console.log(punchline)
+//   // }
+// }
+
+@NgModule({
+  //The list of components or directives belonging to this module.
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegistraionComponent
+  ],
+  //the other angualr modules that export material we need in this angular module.
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+
+  providers: [],
+  //Identifies the root component that Angular should bootstrap when it starts the application.
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/auth/registraion.component.ts
@@ -0,0 +1,109 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup , FormBuilder  , Validator, Validators, AbstractControl} from '@angular/forms';
+
+@Component({
+  selector: 'app-registraion',
+  templateUrl: './registraion.component.html',
+  styleUrls: ['./registraion.component.css']
+})
+export class RegistraionComponent implements OnInit {
+
+  registrationForm : FormGroup;
+  //we need to inject service class at constructor
+  constructor(private fb: FormBuilder) { }
+
+  //Invoked when given component has been initialized.
+//This hook is only called once after the first ngOnChanges
+
+  ngOnInit() {
+    this.registrationForm = this.fb.group({
+      firstName : ['' ,[ Validators.required , Validators.minLength(4) , Validators.maxLength(10) ]
+    ],
+      lastName: ['' , [Validators.required]],
+      email: ['' , [Validators.required , emailDomain('dell.com')]],
+      password: [''],
+      role: ['admin' , [Validators.required]],
+      skills: this.fb.group({
+        skillName: [''],
+        experience: ['']
+      })
+    })
+    //we can observe any form control and group also by valueChanges we can use it at error validation and many task
+    // this.registrationForm.get('firstName').valueChanges.subscribe((value: string)=>{
+    //   console.log(value)
+    // })
+
+    this.registrationForm.valueChanges.subscribe((data: any)=>{
+      this.logKeyValuePayers(this.registrationForm);
+    })
+  }
+
+  //we can keep some validation msg , and checking it for loop we will populate anothe object and will show it at browser
+
+  validationMessages = {
+    'firstName':{
+      'required': 'Full Name is required',
+      'minlength':'minimun length',
+      'maxlength': 'maximum length'
+    },
+    'lastName':{
+      'required': 'Last Name is required',
+    },
+    'email':{
+      'required': 'email is required',
+      'emailDomain': 'you are not varified',
+    },
+    'role':{
+      'required': 'Role is required',
+    }
+  }
+
+  formErrors = {
+    'firstName': '',
+    'lastName':'',
+    'email': '',
+    'role':'',
+  }
+//passing default value here
+  logKeyValuePayers(group: FormGroup = this.registrationForm): void{
+    (Object.keys(group.controls)).forEach((key:string)=>{
+      const abstractControll =  group.get(key);
+      if(abstractControll instanceof FormGroup){
+        this.logKeyValuePayers(abstractControll)
+      }
+      else{
+        this.formErrors[key] = '';
+        // console.log('key =' + key+ 'value =' + abstractControll)
+        if(abstractControll && !abstractControll.valid && (abstractControll.touched || abstractControll.dirty)){
+          const message = this.validationMessages[key];
+          console.log(message)
+          for(const errorKey in abstractControll.errors){
+            console.log(errorKey)
+            if(errorKey){
+              this.formErrors[key] += message[errorKey] + ' ';
+            }
+          }
+        }
+      }
+    })
+  }
+
+  onSubmit(): void{
+    this.logKeyValuePayers(this.registrationForm)
+    console.log(this.formErrors)
+  }
+
+}
+
+function emailDomain(domainName: string){
+  return(control : AbstractControl): {[key: string] : any} | null =>{
+    const email: string = control.value;
+    const domain = email.substring(email.lastIndexOf('@')+1);
+    if(email == '' || domain.toLowerCase() === domainName){
+      return null;
+    }
+    else{
+      return {'emailDomain': true};
+    }
+  }
+}
